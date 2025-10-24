f3d01023a560f382a7f30681e41bbee1727cf9ba
kmiiyaaa <ckm1533@naver.com>
2025-10-24 12:29:51 +0900

    Lombok 설치 및  dependencies 추가 , lombok test용 dto 및 entity 추가

--- src/main/java/com/kmii/home/controller/HomeController.java
+++ src/main/java/com/kmii/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.kmii.home.controller;
 
+import com.kmii.home.dto.Board;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,14 @@ public class HomeController {
     @GetMapping("/test")
     public String test(){
         return "hello";
+    }
+
+    @GetMapping("/board")
+    public Board board() {
+        Board board = new Board();
+        board.setName("홍길동");
+        board.setPass("12345");
+
+        return  board;
     }
 }
---
+++ src/main/java/com/kmii/home/dto/Board.java
@@ -0,0 +1,16 @@
+package com.kmii.home.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+
+    private String name;
+    private String pass;
+}
